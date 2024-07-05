93cec39604016dbb28e944b77b730f3613ae15bf
M.JAVAD <162063682+M20v05J@users.noreply.github.com>
2024-07-05 10:12:56 +0330

    برای اد پست تغیرات ایجاد کردم

--- addpost.h
+++ addpost.h
@@ -14,21 +14,10 @@ class addPost : public QMainWindow
 public:
     explicit addPost(QWidget *parent = nullptr);
     ~addPost();
-
+public slots:
+    void SetTexts();
 private slots:
-    void on_homePushButton_clicked();
 
-    void on_pushButton_2_clicked();
-
-    void on_pushButton_3_clicked();
-
-    void on_pushButton_4_clicked();
-
-    void on_pushButton_5_clicked();
-
-    void on_pushButton_clicked();
-
-    void on_textEdit_textChanged();
 
 private:
     Ui::addPost *ui;
