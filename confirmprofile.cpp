f30693e9aa2f73b387eb2aab720696d3bdc8d9e6
M.JAVAD <162063682+M20v05J@users.noreply.github.com>
2024-07-09 22:54:27 +0330

    ok

--- confirmprofile.cpp
+++ confirmprofile.cpp
@@ -29,7 +29,7 @@ void confirmprofile::on_pushButton_clicked()
     QSqlQuery q;
     QSqlQuery s;
 
-    QString Fname,Lname,birthday,most_recent_job_title,university,most_recent_company,employment_type,skill;
+    QString Fname,Lname,birthday,most_recent_job_title,university,most_recent_company,employment_type;
     Fname=ui->lineEdit_10->text();
     Lname=ui->lineEdit_11->text();
     birthday=ui->lineEdit_12->text();
@@ -37,7 +37,7 @@ void confirmprofile::on_pushButton_clicked()
     university=ui->lineEdit_14->text();
     most_recent_company=ui->lineEdit_15->text();
     employment_type=ui->lineEdit_16->text();
-    skill = ui->lineEdit_18->text();
+
 
 
     while (true) {
@@ -51,7 +51,7 @@ void confirmprofile::on_pushButton_clicked()
         else {
             id_count=id_count-1;
             QString qstring_id = QString::number(id_count);
-            q.exec("UPDATE user SET first_name='"+Fname+"',last_name='"+Lname+"',birthday='"+birthday+"',most_recent_jobtitle='"+most_recent_job_title+"',school_university='"+university+"',most_recent_company='"+most_recent_company+"',employment_type='"+employment_type+"' WHERE id='"+qstring_id+"',skills='"+skill+"'");
+            q.exec("UPDATE user SET first_name='"+Fname+"',last_name='"+Lname+"',birthday='"+birthday+"',most_recent_jobtitle='"+most_recent_job_title+"',school_university='"+university+"',most_recent_company='"+most_recent_company+"',employment_type='"+employment_type+"' WHERE id='"+qstring_id+"'");
             break;
         }
     }
