e482de1452421d6a36def40746cfd590cd94af8a
white-vessel <166604170+white-vessel@users.noreply.github.com>
2024-07-09 06:27:29 +0330

    add content class v1.6

--- post.cpp
+++ post.cpp
@@ -13,13 +13,13 @@ post::post()
 }
 void SETimage_path(QString img){
     QSqlQuery q;
-    q.exec("INSERT INTO current_user(username)VALUES('"+img+"')");
+    q.exec("INSERT INTO post(file_path)VALUES('"+img+"')");
 }
 void SETcaption(QString cpt){
     QSqlQuery q;
-    q.exec("INSERT INTO current_user(username)VALUES('"+cpt+"')");
+    q.exec("INSERT INTO post(caption)VALUES('"+cpt+"')");
 }
 void SETcategory(QString ctg){
     QSqlQuery q;
-    q.exec("INSERT INTO current_user(username)VALUES('"+ctg+"')");
+    q.exec("INSERT INTO post(category)VALUES('"+ctg+"')");
 }
